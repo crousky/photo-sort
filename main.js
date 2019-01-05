fe6a4e98ce1936c9798516230f09153807f6b12c
Ben Crouse <crousky@gmail.com>
2019-01-05 08:57:51 -0500

    progress indicator

--- main.js
+++ main.js
@@ -13,8 +13,12 @@ let mainWindow;
 function createWindow () {
     mainWindow = new BrowserWindow({ width: 1024, height: 768, minWidth: 800, minHeight: 600 });
     //mainWindow.webContents.openDevTools();
+
+    let mainWindowPath = `file://${__dirname}\\views\\index.pug`;
+
+    console.log('loading main window', mainWindowPath);
  
-    mainWindow.loadURL(`file://${__dirname}/views/index.pug`);
+    mainWindow.loadURL(mainWindowPath);
 
     mainWindow.on('closed', function() {
         mainWindow = null;
